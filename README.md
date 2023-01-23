ff4553ac616664dbb8592a04b6ab0a4d02d965f1
Leron Gray <daddycocoaman@gmail.com>
2023-01-23 11:33:33 -0600

    Update README and dllrunner

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # AzBelt
 
 Standalone DLL and sliver extension for enumerating Azure related credentials, primarily on AAD joined machines
+
+A devcontainer is a provided for easy development and building. To build, simply:
+
+``nimble release``
+
+This will drop the DLLs into the project folder. If you want the exe for testing the DLL, you can also build the DLL runner:
+
+``nimble dllrun``
+
+You can do both at the same time:
+
+``nimble all``
--- tools/dllrun.nim
+++ tools/dllrun.nim
@@ -7,10 +7,14 @@ type
   EntryFunction = proc (args: varargs[LPCWSTR]): int {.gcsafe, stdcall.}
 
 let lib = loadLib(paramStr(1))
-assert lib != nil, "Error loading library"
+if lib == nil:
+  echo "Error loading library"
+  quit(1)
 
 let entry = cast[EntryFunction](lib.symAddr("go"))
-assert entry != nil, "Error loading 'entrypoint' function from library"
+if entry == nil:
+  echo "Error loading 'entrypoint' function from library"
+  quit(2)
 
 let args = commandLineParams()[1..^1].mapIt(LPCWSTR(it))
 discard entry(args)
